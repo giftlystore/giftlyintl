79fbeac466e10c81ad584f76ff15e07b2eae5649
giftlystore <trust.wallet.mgt.intl@gmail.com>
2025-12-23 10:43:03 -0800

    Add files via upload

---
+++ GiftMe/listing.js
@@ -0,0 +1,23 @@
+/* DATASET */
+const products=[
+{id:1,name:"Chocolate Basket",cat:"Food",old:150,new:120,img:"https://i.pinimg.com/1200x/51/c8/0d/51c80d48d2a39151447602d2d7030395.jpg"},
+{id:2,name:"Rose Bouquet",cat:"Flowers",old:230,new:180,img:"https://i.pinimg.com/736x/0b/02/98/0b0298cb17629bb6caa6e08e7ded426c.jpg"},
+{id:3,name:"Luxury Watch",cat:"Accessories",old:693,new:550,img:"https://i.pinimg.com/736x/88/1d/a9/881da96edc5a8eea60f037842949f2b5.jpg"},
+{id:4,name:"Birthday Hamper",cat:"Occasion",old:370,new:330,img:"https://i.pinimg.com/1200x/5d/1c/43/5d1c43fa44d4ceec1d095ce415241b0a.jpg"},
+{id:5,name:"Wine Gift Box",cat:"Food",old:518,new:480,img:"https://i.pinimg.com/736x/60/8d/8d/608d8dad72ffa5bfd05222316deb9aec.jpg"},
+{id:6,name:"Teddy Bear",cat:"Kids",old: 642,new:600,img:"https://i.pinimg.com/736x/2f/27/36/2f27364bbac93aa405ed10c2352f5031.jpg"},
+{id:7,name:"Perfume Set",cat:"Accessories",old:430,new:400,img:"https://i.pinimg.com/1200x/d9/71/df/d971df1e3215e6d785837c17c77154ce.jpg"},
+{id:8,name:"Snack Basket",cat:"Food",old:171,new:150,img:"https://i.pinimg.com/1200x/5f/51/1c/5f511c6ee1dd511b53819691adad21db.jpg"},
+{id:9,name:"Flower Vase",cat:"Home",old:400,new:340,img:"https://i.pinimg.com/1200x/d3/08/f1/d308f1420da3473766aeaa0da3325c2c.jpg"},
+{id:10,name:"Couple Gift Set",cat:"Occasion",old:428,new:400,img:"https://i.pinimg.com/736x/67/fa/ae/67faaeae18f3535323747bae3ae261bd.jpg"},
+{id:11,name:"Coffee Box",cat:"Food",old:390,new:380,img:"https://i.pinimg.com/736x/b2/d8/15/b2d815bd6a6ac229a03b4bf116959923.jpg"},
+{id:12,name:"Baby Gift Hamper",cat:"Kids",old:452,new:440,img:"https://i.pinimg.com/1200x/48/b0/79/48b07903362683f89724cc49e705a008.jpg"},
+{id:13,name:"Luxury Candle",cat:"Home",old:294,new:280,img:"https://i.pinimg.com/1200x/23/3d/13/233d1393880fb3b6e9b5a55d11658756.jpg"},
+{id:14,name:"Bracelet",cat:"Accessories",old:620,new:580,img:"https://i.pinimg.com/1200x/27/64/78/276478a70a26d15e3e0931cc483f32fc.jpg"},
+{id:15,name:"Wedding Gift Box",cat:"Occasion",old:523,new:495,img:"https://i.pinimg.com/1200x/33/ce/79/33ce7966f58c32c8639864dd6a3b058b.jpg"},
+{id:16,name:"Chocolate Tower",cat:"Food",old:290,new:270,img:"https://i.pinimg.com/1200x/d1/ec/38/d1ec388a1e064806f29ec9cdee12ffc7.jpg"},
+{id:17,name:"Plush Pillow",cat:"Home",old:310,new:278,img:"https://i.pinimg.com/1200x/c3/8b/9a/c38b9a85ee6366f425ed670e4614f187.jpg"},
+{id:18,name:"Kids Toy Box",cat:"Kids",old:380,new:340,img:"https://i.pinimg.com/1200x/f1/bb/42/f1bb42cd13d21680885768c0b2df86fe.jpg"},
+{id:19,name:"Flower Basket",cat:"Flowers",old:191,new:184,img:"https://i.pinimg.com/736x/7a/bc/46/7abc460d1932811efdab839e0ee4e2a4.jpg"},
+{id:20,name:"Premium Gift Set",cat:"Occasion",old:240,new:230,img:"https://i.pinimg.com/1200x/e8/b5/a2/e8b5a2c5ea8bbec1ec11eda49676399f.jpg"}
+];
